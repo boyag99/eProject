0a359e48895b4f05d330ff3366a65e66a95d5e6e
Trinh Hao Hiep <trinhhaohiep26071999@gmail.com>
2020-05-22 23:39:04 +0700

    add data for header cart and popup cart

---
+++ eProject/ViewComponents/HeaderCartViewComponent.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eProject.App.Helpers;
+using eProject.Data;
+using eProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eProject.ViewComponents
+{
+    [ViewComponent(Name = "HeaderCart")]
+    public class HeaderCartViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public HeaderCartViewComponent(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            if (SessionHelper.GetObjectFromJson<List<Item>>(HttpContext.Session, "cart") == null)
+            {
+                List<Product> products = new List<Product>();
+                ViewBag.Products = products;
+            }
+            else
+            {
+                List<Product> products = new List<Product>();
+                List<Item> carts = SessionHelper.GetObjectFromJson<List<Item>>(HttpContext.Session, "cart");
+                ViewBag.CountItems = carts.Count;
+                ViewBag.Total = carts.Sum(it => it.Price);
+
+                foreach (Item item in carts)
+                {
+                    Product product = await _applicationDbContext.Products.Include(p => p.Photos).FirstOrDefaultAsync(p => p.ProductId == item.ItemId);
+
+                    products.Add(product);
+                }
+
+                ViewBag.Products = products;
+               
+            }
+
+            return View("Index");
+        }
+    }
+}
