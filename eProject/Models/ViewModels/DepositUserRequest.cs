e49f0c4f187677d76d5ea2c234a492dd4cb6dfa8
Tuyen Tran <tuyentran8121997@gmail.com>
2020-05-23 16:43:54 +0700

    Commit final

---
+++ eProject/Models/ViewModels/DepositUserRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eProject.Models.ViewModels
+{
+    public class DepositUserRequest
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public double Balance { get; set; }
+    }
+}
