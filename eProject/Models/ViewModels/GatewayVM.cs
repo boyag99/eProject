a80517299f3fed8cf4cb5d82bbfaee1c3989019c
Trinh Hao Hiep <trinhhaohiep26071999@gmail.com>
2020-05-23 11:27:39 +0700

    Add gateway and payment

---
+++ eProject/Models/ViewModels/GatewayVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace eProject.Models.ViewModels
+{
+    public class GatewayVM
+    {
+        public List<Gateway> Gateways { get; set; }
+
+        public Gateway Gateway { get; set; }
+    }
+}
